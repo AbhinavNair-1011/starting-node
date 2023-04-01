f7e540bf37d22143ffd15c490a1ab3c2dd890700
AbhinavNair-1011 <abhivish1011@gmail.com>
2023-04-01 15:50:25 +0530

    res spread and .map

--- index.js
+++ index.js
@@ -4,10 +4,35 @@
 console.log("hello sir");
 
 let result=(a,b)=>{
-return `sum - ${a+b}`
+return `\nsum - ${a+b}`
 }
 
 let studentObj={name:"abhinav" ,age:21}
 
 console.log(`my name is ${studentObj.name} and age is ${studentObj.age}`)
 console.log(result(1,2))
+
+// task 3
+let array1= ["apple","orange","","mango","","lemon"];
+
+let array2= array1.map((each)=>{
+    if(each===""){
+        each="empty string"
+    }
+    return each
+})
+console.log(`old array-${array1} \n\nmodified new array-${array2}\n`);
+
+//spread op
+
+let array3= [...array2];
+console.log("used spread op " +array3)
+
+//rest op
+ function a(...rest){
+//     for(let each of rest){
+// console.log(each)
+//     }
+    console.log( "used rest op"+rest);
+ }
+ a(1,2,3,4,5,6,7,8,9)
