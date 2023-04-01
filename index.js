6cca6f8df765d0f667a475f51b4e5dd02b39d315
AbhinavNair-1011 <abhivish1011@gmail.com>
2023-04-01 12:41:34 +0530

    task 2

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 //run node index.js in terminal , node must be installed 
 
+
 console.log("hello sir");
+
+let result=(a,b)=>{
+return `sum - ${a+b}`
+}
+
+let studentObj={name:"abhinav" ,age:21}
+
+console.log(`my name is ${studentObj.name} and age is ${studentObj.age}`)
+console.log(result(1,2))
