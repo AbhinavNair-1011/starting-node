983fdf86ad0ac0e9b7ee91109f47c0a2e6ddd82f
AbhinavNAir-1011 <abhivish1011@gmail.com>
2023-04-03 01:16:39 +0530

    created a server

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-//run node index.js in terminal , node must be installed 
+// //run node index.js in terminal , node must be installed 
 
 
-console.log("hello sir");
+// console.log("hello sir");
 
-let result=(a,b)=>{
-return `\nsum - ${a+b}`
-}
+// let result=(a,b)=>{
+// return `\nsum - ${a+b}`
+// }
 
-let studentObj={name:"abhinav" ,age:21}
+// let studentObj={name:"abhinav" ,age:21}
 
-console.log(`my name is ${studentObj.name} and age is ${studentObj.age}`)
-console.log(result(1,2))
+// console.log(`my name is ${studentObj.name} and age is ${studentObj.age}`)
+// console.log(result(1,2))
 
-// task 3
-let array1= ["apple","orange","","mango","","lemon"];
+// // task 3
+// let array1= ["apple","orange","","mango","","lemon"];
 
-let array2= array1.map((each)=>{
-    if(each===""){
-        each="empty string"
-    }
-    return each
-})
-console.log(`old array-${array1} \n\nmodified new array-${array2}\n`);
-
-//spread op
-
-let array3= [...array2];
-console.log("used spread op " +array3)
-
-//rest op
- function a(...rest){
-//     for(let each of rest){
-// console.log(each)
+// let array2= array1.map((each)=>{
+//     if(each===""){
+//         each="empty string"
 //     }
-    console.log( "used rest op"+rest);
- }
- a(1,2,3,4,5,6,7,8,9)
+//     return each
+// })
+// console.log(`old array-${array1} \n\nmodified new array-${array2}\n`);
+
+// //spread op
+
+// let array3= [...array2];
+// console.log("used spread op " +array3)
+
+// //rest op
+//  function a(...rest){
+// //     for(let each of rest){
+// // console.log(each)
+// //     }
+//     console.log( "used rest op"+rest);
+//  }
+//  a(1,2,3,4,5,6,7,8,9)
+
+
+
+ //creating a server 
+
+ let http=require("http")
+ let server= http.createServer((req,res)=>{
+    console.log(req)
+    
+ })
+ server.listen(3000)
